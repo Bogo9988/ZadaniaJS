fad28e1ecbd83c53200e2b4e67cf6477f75da7fc
Bogo9988 <7szymo@gmail.com>
2024-12-01 20:01:37 +0100

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,28 +1,28 @@
-//tworzymy tablice
-const products = [
-    { name: "Laptop", price: 3000, category: "elektronika" },
-    { name: "Telefon", price: 1500, category: "elektronika" },
-    { name: "Stol", price: 200, category: "meble" },
-    { name: "Krzeslo", price: 100, category: "meble" },
+const produkty = [
+    { nazwa: "Laptop", cena: 3000, kategoria: "elektronika" },
+    { nazwa: "Telefon", cena: 1500, kategoria: "elektronika" },
+    { nazwa: "Stol", cena: 200, kategoria: "meble" },
+    { nazwa: "Krzeslo", cena: 100, kategoria: "meble" },
 ];
 
-const filterProducts = (products, category = null, minPrice = 0, maxPrice = 10000) =>
-    products.filter(
-        //destrukturyzacja aby wyciagnac category i price
-        ({ category: c, price }) =>
-            (!category || c === category) && price >= minPrice && price <= maxPrice
+const filtrowanieProduktow = (produkty, kategoria = null, minCena = 0, maxCena = 10000) =>
+    produkty.filter(
+        // Destrukturyzacja chcemy wyciagnac kategoria cen
+        ({ kategoria: c, cena }) =>
+            (!kategoria || c === kategoria) && cena >= minCena && cena <= maxCena
     );
 
-function displayProducts(filteredProducts) {
-    filteredProducts.forEach(product => {
-        console.log(`Produkt: ${product.name}, Cena: ${product.price} PLN, Kategoria: ${product.category}`);
+function wyswietlProdukty(filtracjaProduktow) {
+    filtracjaProduktow.forEach(produkt => {
+        console.log(` Produkt: ${produkt.nazwa}, Cena: ${produkt.cena}, Kategoria: ${produkt.kategoria} `);
     });
 }
 
-const categoryFilter = "elektronika";
-const minPriceFilter = 1000;         
-const maxPriceFilter = 3500;       
+const kategoriaFiltru = "elektronika";
+const minCenaFiltru = 1000;         
+const maxCenaFiltru = 3500;       
 
-const filteredProducts = filterProducts(products, categoryFilter, minPriceFilter, maxPriceFilter);
 
-displayProducts(filteredProducts);
+const filtryProduktow = filtrowanieProduktow(produkty, kategoriaFiltru, minCenaFiltru, maxCenaFiltru);
+
+wyswietlProdukty(filtryProduktow);
