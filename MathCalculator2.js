076476c7853bfc945f6876b74ee6cb6be3b7ef0f
Bogo9988 <7szymo@gmail.com>
2024-12-01 20:50:08 +0100

    Update MathCalculator2.js

--- MathCalculator2.js
+++ MathCalculator2.js
@@ -1,34 +1,28 @@
-// klasa MatematycznyKalkulator
 class MatematycznyKalkulator {
     // konstruktor uruchamia program do czysczenia i wyswietlania histori
     constructor(){
         this.history = [];
     }
-    // dodawanie liczb
+    
     dodawanie = (...numbers) => {
         const result = numbers.reduce((sum,num)=> sum + num, 0)
         this.history.push(`dodawanie: ${numbers.join(" + ")} = ${result}`)
         return result
     }
-
-    // odejmowanie liczb
+    
     odejmowanie = (a, b) => {
         const result = a - b
         this.history.push(`odejmowanie: ${a} - ${b} = ${result}`)
         return result
     };
-
-    // mnożenie liczb
+    
     mnozenie = (...numbers) => {
         const result = numbers.reduce((mn,num)=> mn * num, 1)
         this.history.push(`mnożenie: ${numbers.join(" * ")} = ${result}`)
         return result
     }
 
-    // dzielenie liczb
     dzielenie = (a, b) => {
-        // wyswietla kominukat bledu jezeli ktos chce podzielic przez zero
-        if (b === 0) throw new Error("nie mozzna dzielic przez zero")
             const result = a / b
         this.history.push(`dzielenie: ${a} / ${b} = ${result}`)
         return result
@@ -41,9 +35,7 @@ class MatematycznyKalkulator {
     czyszczenieHistorii = () => {this.history = []}
 
 }
-
-  // przyklady
-  const kalkulator = new MatematycznyKalkulator();
+const kalkulator = new MatematycznyKalkulator();
   console.log(kalkulator.dodawanie(5,5))
   console.log(kalkulator.odejmowanie(5,5))
   console.log(kalkulator.mnozenie(5,5))
